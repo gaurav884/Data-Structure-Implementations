5238a29de4df5bebc54fd851f0bf13c79c5ccc03
Gaurav <tgaurav053@gmail.com>
2023-06-28 07:45:33 +0530

    heap modified

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -2,131 +2,147 @@
 
 using namespace std;
 
-class Node{
-   public:
+class Node
+{
+
+private:
     int val;
-    Node* next;
+    Node *next;
+    friend class LinkedList;
 
-   
-    Node(int node_val){
-         val = node_val;
-         next = NULL;
+    Node(int node_val)
+    {
+        val = node_val;
+        next = NULL;
     }
-
 };
 
-class LinkedList{
-    public:
-    Node* head;
+class LinkedList
+{
+public:
+    Node *head;
 
-    LinkedList(int head_val){
+    LinkedList(int head_val)
+    {
         head = new Node(head_val);
     }
 
-    void insert(int node_val){
+    void insert(int node_val)
+    {
 
-        if(head == NULL){
+        if (head == NULL)
+        {
             head = new Node(node_val);
         }
-        else{
-            Node* curr = head;
+        else
+        {
+            Node *curr = head;
 
-            while(curr->next != NULL){
+            while (curr->next != NULL)
+            {
                 curr = curr->next;
             }
 
             curr->next = new Node(node_val);
-
         }
     }
 
-    void removeBack(){
-        
-        if(head == NULL){
-            cout<<"Linked List is Empty"<<endl;
+    void removeBack()
+    {
+
+        if (head == NULL)
+        {
+            cout << "Linked List is Empty" << endl;
             return;
         }
-        else if(head->next ==NULL){
+        else if (head->next == NULL)
+        {
             head = NULL;
         }
-        else{
-            Node* curr = head;
+        else
+        {
+            Node *curr = head;
 
-            while(curr->next->next != NULL){
+            while (curr->next->next != NULL)
+            {
                 curr = curr->next;
             }
             curr->next = NULL;
- 
         }
-
     }
 
-    void removeFront(){
-        
-        if(head == NULL){
-            cout<<"Linked List is Empty"<<endl;
+    void removeFront()
+    {
+
+        if (head == NULL)
+        {
+            cout << "Linked List is Empty" << endl;
             return;
         }
-        else{
-           
-           Node* temp = head;
-           head = head->next;
-           temp=NULL;
- 
-        }
+        else
+        {
 
+            Node *temp = head;
+            head = head->next;
+            temp = NULL;
+        }
     }
 
-    void printList(){
+    void printList()
+    {
 
-        if(head == NULL){
-            cout<<"Linked List is empty";
+        if (head == NULL)
+        {
+            cout << "Linked List is empty";
         }
-        else{     
-            Node* curr = head;      
-            while(curr != NULL){
+        else
+        {
+            Node *curr = head;
+            while (curr != NULL)
+            {
 
-                cout<<curr->val<<" ";
+                cout << curr->val << " ";
                 curr = curr->next;
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-    
-    int size(){
 
-        if(head == NULL){
+    int size()
+    {
+
+        if (head == NULL)
+        {
             return 0;
         }
-        else{
+        else
+        {
             int count = 0;
-            Node* curr = head;
+            Node *curr = head;
 
-            while(curr != NULL){
+            while (curr != NULL)
+            {
                 count++;
                 curr = curr->next;
             }
 
             return count;
-
         }
     }
-    
-    
 };
 
-int main(){
+int main()
+{
 
-      LinkedList* a = new LinkedList(4);
+    LinkedList *a = new LinkedList(4);
 
-      a->insert(5);
-      a->insert(7);
-      a->insert(9);
-      a->insert(10);
-      a->removeFront();
-      a->printList();
-      cout<<a->size();
-
+    a->insert(5);
+    a->insert(7);
+    a->insert(9);
+    a->insert(10);
+    a->removeFront();
+    a->printList();
+    cout << a->size();
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-
-        if(rear == -1 && front == -1){
-            rear , front = 1;
-            temp[front]= val;
-        }
