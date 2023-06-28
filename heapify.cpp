3e1cb1c8799d248d8cf949a9bd65dce7215984f7
Gaurav <tgaurav053@gmail.com>
2023-06-28 07:51:12 +0530

    heapify modified

--- heapify.cpp
+++ heapify.cpp
@@ -2,43 +2,36 @@
 
 using namespace std;
 
+void heapify(int arr[], int i, int size)
+{
 
-void heapify(int arr[] , int i , int size){
-    
-    int left = 2*i+1;
-    int right = 2*i+2;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
     int replaceWith = i;
 
-    
-    if(left >= size && right >= size) return ;
-   
-    
-    if(left<size && right< size ){
+    if ((left >= size && right >= size) || (arr[i] > arr[left] && arr[i] > arr[right]))
+        return;
 
-        replaceWith = arr[left]>arr[right] ? left : right;
-    }
+    replaceWith = arr[left] > arr[right] ? left : right;
 
-    else replaceWith = left < size ? left : right;
-
-   
-    swap(arr[i] , arr[replaceWith]);
-   
-
+    swap(arr[i], arr[replaceWith]);
 }
 
+int main()
+{
 
-int main(){
+    int arr[7] = {-1, 54, 53, 55, 52, 50 , 60};
+    int n = 7;
 
-    int arr[6] = {-1 , 54 ,53 ,55 , 52 ,50};
-    int n= 5;
-    
-    for(int i=n/2 ; i>=0 ;i--){
-        heapify(arr , i , n);
+    for (int i = n / 2; i >= 0; i--)
+    {
+        heapify(arr, i, n);
     }
-    
-    for(int i =0 ;i<n ;i++){
-        cout<<arr[i]<<" ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
     return 0;
 }
